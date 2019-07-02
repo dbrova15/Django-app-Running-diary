6d186bd6ebf0316c8d45a7c022d07b4f6182edda
dimon <smm@bbukva.com>
2019-07-02 23:50:10 +0300

    edit local setting 02 07 2

--- myapp/local_settings.py
+++ myapp/local_settings.py
@@ -5,7 +5,7 @@ LOCAL_SERV = PROD_SERV = TEST_SERV = False
 test_host = []
 deploy_host = ['green-liveconsole3']
 if socket.gethostname() in deploy_host:
-    PROD = True
+    DEBUG = False
     PROD_SERV = True
 elif socket.gethostname() in test_host:
     TEST_SERV = True

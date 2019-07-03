5bcb3415b6f21e4067d49ca292b1bbdc24d042d6
dimon <smm@bbukva.com>
2019-07-03 11:01:46 +0300

    deploy 03 07 3

--- Running_diary/settings.py
+++ Running_diary/settings.py
@@ -143,7 +143,7 @@ except ImportError:
 
     sys.stderr.write(
         "Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    # sys.exit(1)
+    sys.exit(1)
 
 if LOCAL_SERV:
     # Database
